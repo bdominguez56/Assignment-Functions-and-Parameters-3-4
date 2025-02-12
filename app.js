ecd1c99ab698f1c368514f47fe58e1ea63c61353
bdominguez56 <111765600+bdominguez56@users.noreply.github.com>
2025-02-11 23:19:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ function greetUser(name) {
 // call the function and pass an argument
 greetUser("Name1");  
 greetUser("Name2");   
+
+// function definition
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// function invocation
+let sum = addNumbers(5, 3);  // sum is 8
+console.log("The sum is: " + sum);  // sum is 8
